0f67ae7057abb65d560899af51176c22bc8368b5
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2023-12-14 13:14:57 -0300

    Ajustando menu javascript

--- js/script.js
+++ js/script.js
@@ -12,3 +12,10 @@ toggleMenu.addEventListener('click', ()=>{
 closeMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('show')
 })
+
+// Remove Menu
+const navLink = document.querySelectorAll('.nav-link')
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
